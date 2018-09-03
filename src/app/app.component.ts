2db44d6485ffcec5316066a0b3d72ca1c0c155d6
Ali Usman <ali.usman@indusvalleylabs.com>
2018-09-03 14:06:30 +0500

    Implemented routing at the root level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'nothing';
-  feature = 'recipes' ;
 
-  updateFeature(feature: string) {
-    this.feature = feature;
-  }
 }
