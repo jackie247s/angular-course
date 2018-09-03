265eb1b68facd72b989f6079dd10236474e78735
Ali Usman <ali.usman@indusvalleylabs.com>
2018-09-03 15:51:37 +0500

    Data passing is now router based. Added new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { SelectRecipeComponent } from './recipes/recipe-detail/select-recipe/select-recipe.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 const routes: Routes = [
-    { path: 'recipes', component: RecipesComponent },
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: SelectRecipeComponent, pathMatch: 'full'},
+        { path: 'new', component: RecipeEditComponent },
+        { path: ':index', component: RecipeDetailComponent },
+        { path: ':index/edit', component: RecipeEditComponent }
+    ] },
     { path: 'shopping-list', component: ShoppingListComponent },
     { path: '', redirectTo: 'recipes', pathMatch: 'full' }
 ];
--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,12 +9,9 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() id: number;
 
-  onSelect() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
-
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
   }
