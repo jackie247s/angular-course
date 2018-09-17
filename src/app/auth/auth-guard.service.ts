e4b7721185778a9ca1f0de103ebc9720ea101246
Ali Usman <ali.usman@indusvalleylabs.com>
2018-09-17 17:03:37 +0500

    Changed behaviour of auth guard to navigate to sign in page if not authenticated

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,12 +1,17 @@
-import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate, Router } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private authService: AuthService) {}
+    constructor(private router: Router, private authService: AuthService) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.authService.isAuthenticated();
+        if (!this.authService.isAuthenticated()) {
+            this.router.navigate(['signin']);
+            return false;
+        }
+
+        return true;
     }
 }
