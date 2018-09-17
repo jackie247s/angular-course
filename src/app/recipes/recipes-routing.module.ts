148b7599b000963bf87cdc97f01bf40ad5626b98
Ali Usman <ali.usman@indusvalleylabs.com>
2018-09-17 15:50:13 +0500

    Added separate recipes module

---
+++ src/app/recipes/recipes-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { SelectRecipeComponent } from './recipe-detail/select-recipe/select-recipe.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { AuthGuard } from '../auth/auth-guard.service';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+
+const recipesRoutes: Routes = [
+    { path: 'recipes', component: RecipesComponent, children: [
+            { path: '', component: SelectRecipeComponent, pathMatch: 'full' },
+            { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
+            { path: ':index', component: RecipeDetailComponent },
+            { path: ':index/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
+        ]
+    },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(recipesRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class RecipesRoutingModule {}
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipesComponent } from './recipes.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { SelectRecipeComponent } from './recipe-detail/select-recipe/select-recipe.component';
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent,
+        SelectRecipeComponent,
+        RecipeEditComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
+})
+export class RecipesModule { }
